53f5dca3f4dec4163f68059085f680ba66fedf4e
Rody2001 <rody@uni-bremen.de>
2025-11-21 15:19:21 +0100

    [test_suturo_lab] typo

--- test/test_world/test_suturo_lab.py
+++ test/test_world/test_suturo_lab.py
@@ -1,7 +1,7 @@
 import pytest
 from semantic_digital_twin.world import World
 from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
-from suturo_resources.suturo_map import load_environment, build_environment_walls, build_environment_furnicher
+from suturo_resources.suturo_map import load_environment, build_environment_walls, build_environment_furniture
 
 def test_load_environment_returns_world():
     world = load_environment()
