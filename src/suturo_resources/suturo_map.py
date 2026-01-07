f13e375798ac8736ee7ad56c4e82cf36882f2954
Rody2001 <rody@uni-bremen.de>
2026-01-07 17:29:07 +0100

    [queries, suturo_map, test_suturo_lab] updated transformation matrix to HomogeneousTransformationMatrix, refined query logic and imports, and adjusted test assertions.

--- src/suturo_resources/suturo_map.py
+++ src/suturo_resources/suturo_map.py
@@ -4,7 +4,7 @@ from semantic_digital_twin.adapters.viz_marker import VizMarkerPublisher
 import threading
 import rclpy
 from semantic_digital_twin.datastructures.prefixed_name import PrefixedName
-from semantic_digital_twin.spatial_types.spatial_types import TransformationMatrix, Point3
+from semantic_digital_twin.spatial_types.spatial_types import HomogeneousTransformationMatrix, Point3
 from semantic_digital_twin.world_description.world_entity import Body
 from semantic_digital_twin.world_description.connections import FixedConnection
 from semantic_digital_twin.world_description.geometry import Box, Scale, Color
@@ -29,7 +29,7 @@ def load_environment():
     root = Body(name=PrefixedName("root"))
 
     root_slam_C_root = FixedConnection(parent=root_slam, child=root,
-                                      parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=0.55,y=0.18,yaw=0.15707963267))
+                                      parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=0.55,y=0.18,yaw=0.15707963267))
     with world.modify_world():
         world.add_connection(root_slam_C_root)
 
@@ -53,7 +53,7 @@ def build_environment_walls(world: World):
     south_wall1_body = Body(name=PrefixedName("south_wall1_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall1 = FixedConnection(parent=root, child=south_wall1_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(y=-2.01, z=1.50))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(y=-2.01, z=1.50))
     all_wall_connections.append(root_C_south_wall1)
 
     south_wall2 = Box(scale=Scale(0.29, 0.05, 3.00), color=gray)
@@ -61,7 +61,7 @@ def build_environment_walls(world: World):
     south_wall2_body = Body(name=PrefixedName("south_wall2_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall2 = FixedConnection(parent=root, child=south_wall2_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=-0.145, y=-1.45, z=1.50))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=-0.145, y=-1.45, z=1.50))
     all_wall_connections.append(root_C_south_wall2)
 
     south_wall3 = Box(scale=Scale(0.05, 1.085, 1.00), color=gray)
@@ -69,7 +69,7 @@ def build_environment_walls(world: World):
     south_wall3_body = Body(name=PrefixedName("south_wall3_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall3 = FixedConnection(parent=root, child=south_wall3_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=-0.29, y=-0.9925, z=0.5))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=-0.29, y=-0.9925, z=0.5))
     all_wall_connections.append(root_C_south_wall3)
 
     south_wall4 = Box(scale=Scale(0.29, 0.05, 1.00), color=gray)
@@ -77,7 +77,7 @@ def build_environment_walls(world: World):
     south_wall4_body = Body(name=PrefixedName("south_wall4_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall4 = FixedConnection(parent=root, child=south_wall4_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=-0.145, y=-0.45, z=0.5))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=-0.145, y=-0.45, z=0.5))
     all_wall_connections.append(root_C_south_wall4)
 
     south_wall5 = Box(scale=Scale(0.29, 0.05, 1.00), color=gray)
@@ -85,7 +85,7 @@ def build_environment_walls(world: World):
     south_wall5_body = Body(name=PrefixedName("south_wall5_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall5 = FixedConnection(parent=root, child=south_wall5_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=-0.145, y=0.45, z=0.5))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=-0.145, y=0.45, z=0.5))
     all_wall_connections.append(root_C_south_wall5)
 
     south_wall6 = Box(scale=Scale(0.05, 2.75, 1.00), color=gray)
@@ -93,7 +93,7 @@ def build_environment_walls(world: World):
     south_wall6_body = Body(name=PrefixedName("south_wall6_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall6 = FixedConnection(parent=root, child=south_wall6_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=-0.29025, y=1.80, z=0.5))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=-0.29025, y=1.80, z=0.5))
     all_wall_connections.append(root_C_south_wall6)
 
     south_wall7 = Box(scale=Scale(0.05, 2.27, 1.00), color=gray)
@@ -101,7 +101,7 @@ def build_environment_walls(world: World):
     south_wall7_body = Body(name=PrefixedName("south_wall7_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_south_wall7 = FixedConnection(parent=root, child=south_wall7_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=-0.29025, y=5.16, z=0.5))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=-0.29025, y=5.16, z=0.5))
     all_wall_connections.append(root_C_south_wall7)
 
     east_wall = Box(scale=Scale(4.924, 0.05, 3.00), color=gray)
@@ -109,7 +109,7 @@ def build_environment_walls(world: World):
     east_wall_body = Body(name=PrefixedName("east_wall_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_east_wall = FixedConnection(parent=root, child=east_wall_body,
-                                   parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=2.462, y=-2.535, z=1.50))
+                                   parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=2.462, y=-2.535, z=1.50))
     all_wall_connections.append(root_C_east_wall)
 
     middle_wall = Box(scale=Scale(0.05, 2.67, 1.00), color=gray)
@@ -117,7 +117,7 @@ def build_environment_walls(world: World):
     middle_wall_body = Body(name=PrefixedName("middle_wall_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_middle_wall = FixedConnection(parent=root, child=middle_wall_body,
-                                   parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=2.20975, y=5.00, z=0.50))
+                                   parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=2.20975, y=5.00, z=0.50))
     all_wall_connections.append(root_C_middle_wall)
 
     west_wall = Box(scale=Scale(4.449, 0.05, 3.00), color=gray)
@@ -125,7 +125,7 @@ def build_environment_walls(world: World):
     west_wall_body = Body(name=PrefixedName("west_wall_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_west_wall = FixedConnection(parent=root, child=west_wall_body,
-                                   parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=1.9345, y=6.32, z=1.50))
+                                   parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=1.9345, y=6.32, z=1.50))
     all_wall_connections.append(root_C_west_wall)
 
     north_wall = Box(scale=Scale(0.05, 8.04, 3.00), color=gray)
@@ -133,7 +133,7 @@ def build_environment_walls(world: World):
     north_wall_body = Body(name=PrefixedName("north_wall_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_north_wall = FixedConnection(parent=root, child=north_wall_body,
-                                   parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=4.949, y=1.51, z=1.50))
+                                   parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=4.949, y=1.51, z=1.50))
     all_wall_connections.append(root_C_north_wall)
 
     north_west_wall = Cylinder(width=1.53, height=3.00, color=gray)
@@ -141,7 +141,7 @@ def build_environment_walls(world: World):
     north_west_wall_body = Body(name=PrefixedName("north_west_wall_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_north_west_wall = FixedConnection(parent=root, child=north_west_wall_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=4.924, y=6.295, z=1.50))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=4.924, y=6.295, z=1.50))
     all_wall_connections.append(root_C_north_west_wall)
 
     with world.modify_world():
@@ -164,7 +164,7 @@ def build_environment_furniture(world: World):
     trash_can_body = Body(name=PrefixedName("trash_can_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_trash_can = FixedConnection(parent=root, child=trash_can_body,
-                                       parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=1, y=1, z=0.20))
+                                       parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=1, y=1, z=0.20))
     all_elements_connections.append(root_C_trash_can)
 
     refrigerator = Box(scale=Scale(0.60, 0.658, 1.49), color=white)
@@ -172,7 +172,7 @@ def build_environment_furniture(world: World):
     refrigerator_body = Body(name=PrefixedName("refrigerator_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_fridge = FixedConnection(parent=root, child=refrigerator_body,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=0.537, y=-2.181,
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=0.537, y=-2.181,
                                                                                                      z=0.745))
     all_elements_connections.append(root_C_fridge)
 
@@ -181,7 +181,7 @@ def build_environment_furniture(world: World):
     counterTop_body = Body(name=PrefixedName("counterTop_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_counterTop = FixedConnection(parent=root, child=counterTop_body,
-                                        parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=1.859,y=-2.181, z=0.2725))
+                                        parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=1.859,y=-2.181, z=0.2725))
     all_elements_connections.append(root_C_counterTop)
 
     ovenArea = Box(scale=Scale(1.20, 0.658, 1.49), color=white)
@@ -189,7 +189,7 @@ def build_environment_furniture(world: World):
     ovenArea_body = Body(name=PrefixedName("ovenArea_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_ovenArea = FixedConnection(parent=root, child=ovenArea_body,
-                                      parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=3.481,y=-2.181, z=0.745))
+                                      parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=3.481,y=-2.181, z=0.745))
     all_elements_connections.append(root_C_ovenArea)
 
     table = Box(scale=Scale(2.45, 0.796, 0.845), color=white)
@@ -197,7 +197,7 @@ def build_environment_furniture(world: World):
     table_body = Body(name=PrefixedName("table_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_table = FixedConnection(parent=root, child=table_body,
-                                   parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=3.545, y=0.426, z=0.4225))
+                                   parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=3.545, y=0.426, z=0.4225))
     all_elements_connections.append(root_C_table)
 
     sofa = Box(scale=Scale(1.68, 0.94, 0.68), color=wood)
@@ -205,7 +205,7 @@ def build_environment_furniture(world: World):
     sofa_body = Body(name=PrefixedName("sofa_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_sofa = FixedConnection(parent=root, child=sofa_body,
-                                  parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=3.60, y=1.20, z=0.34))
+                                  parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=3.60, y=1.20, z=0.34))
     all_elements_connections.append(root_C_sofa)
 
     lowerTable = Box(scale=Scale(0.37, 0.91, 0.44), color=white)
@@ -213,7 +213,7 @@ def build_environment_furniture(world: World):
     lowerTable_body = Body(name=PrefixedName("lowerTable_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_lowerTable = FixedConnection(parent=root, child=lowerTable_body,
-                                        parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=4.22, y=2.22, z=0.22))
+                                        parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=4.22, y=2.22, z=0.22))
     all_elements_connections.append(root_C_lowerTable)
 
     cabinet = Box(scale=Scale(0.43, 0.80, 2.02), color=white)
@@ -221,7 +221,7 @@ def build_environment_furniture(world: World):
     cabinet_body = Body(name=PrefixedName("cabinet_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_cabinet = FixedConnection(parent=root, child=cabinet_body,
-                                     parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=4.65, y=4.72, z=1.01))
+                                     parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=4.65, y=4.72, z=1.01))
     all_elements_connections.append(root_C_cabinet)
 
 
@@ -230,7 +230,7 @@ def build_environment_furniture(world: World):
     desk_body = Body(name=PrefixedName("desk_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_desk = FixedConnection(parent=root, child=desk_body,
-                                  parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=0.05, y=1.48, z=0.375))
+                                  parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=0.05, y=1.48, z=0.375))
     all_elements_connections.append(root_C_desk)
 
     cookingTable = Box(scale=Scale(1.75, 0.64, 0.71),color=wood)
@@ -238,7 +238,7 @@ def build_environment_furniture(world: World):
     cookingTable_body = Body(name=PrefixedName("cookingTable_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_cookingTable = FixedConnection(parent=root,child=cookingTable_body,
-                                  parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=1.325, y=5.675, z=0.355))
+                                  parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=1.325, y=5.675, z=0.355))
     all_elements_connections.append(root_C_cookingTable)
 
 
@@ -247,7 +247,7 @@ def build_environment_furniture(world: World):
     diningTable_body = Body(name=PrefixedName("diningTable_body"), collision=shape_geometry, visual=shape_geometry)
 
     root_C_diningTable = FixedConnection(parent=root,child=diningTable_body,
-                                         parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=2.59975, y=5.705, z=0.365))
+                                         parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=2.59975, y=5.705, z=0.365))
     all_elements_connections.append(root_C_diningTable)
 
     kitchen_floor = [
@@ -282,7 +282,7 @@ def build_environment_furniture(world: World):
     kitchen_world = RoomFactory(name=PrefixedName("kitchen_room"), floor_polytope=kitchen_floor).create()
 
     root_C_kitchen = FixedConnection(parent=root, child=kitchen_world.root,
-                                  parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=2.317, y=-0.843))
+                                  parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=2.317, y=-0.843))
     with world.modify_world():
         world.merge_world(kitchen_world, root_C_kitchen)
 
@@ -290,7 +290,7 @@ def build_environment_furniture(world: World):
     living_room_world = RoomFactory(name=PrefixedName("living_room"), floor_polytope=living_room_floor).create()
 
     root_C_living = FixedConnection(parent=root, child=living_room_world.root,
-                                  parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=2.317, y=2.3095))
+                                  parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=2.317, y=2.3095))
     with world.modify_world():
         world.merge_world(living_room_world, root_C_living)
 
@@ -298,7 +298,7 @@ def build_environment_furniture(world: World):
     bed_room_world = RoomFactory(name=PrefixedName("bed_room"), floor_polytope=bed_room_floor).create()
 
     root_C_bed = FixedConnection(parent=root, child=bed_room_world.root,
-                                    parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=0.96, y=4.96))
+                                    parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=0.96, y=4.96))
     with world.modify_world():
         world.merge_world(bed_room_world, root_C_bed)
 
@@ -306,7 +306,7 @@ def build_environment_furniture(world: World):
     office_world = RoomFactory(name=PrefixedName("office"), floor_polytope=office_floor).create()
 
     root_C_office = FixedConnection(parent=root, child=office_world.root,
-                                 parent_T_connection_expression=TransformationMatrix.from_xyz_rpy(x=3.56, y=4.96))
+                                 parent_T_connection_expression=HomogeneousTransformationMatrix.from_xyz_rpy(x=3.56, y=4.96))
     with world.modify_world():
         world.merge_world(office_world, root_C_office)
 
